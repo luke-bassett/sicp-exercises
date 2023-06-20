caaaf5602ab7d8514cbfe80266b040ee771cc399
Luke Bassett <bassett.luke@gmail.com>
2023-06-20 13:27:51 -0700

    improved 1-12

--- 1-12.scm
+++ 1-12.scm
@@ -1,19 +1,27 @@
 #lang sicp
 (define (pascals-triangle row column)
-  (cond ((or (= row 1) (= row 2)) 1)
-        ((or (= column 1) (= column row)) 1)
+  (cond ((or (= row 0) (= row 1)) 1)
+        ((or (= column 0) (= column row)) 1)
         (else (+ 
-                (pascals-triangle (- row 1) (- column 1)) 
-                (pascals-triangle (- row 1) column)
+                (pascals-triangle (dec row) (dec column)) 
+                (pascals-triangle (dec row) column)
                 ))))
 
+(pascals-triangle 0 0)
+;1
 (pascals-triangle 1 1)
 ;1
-(pascals-triangle 2 2)
-;1
-(pascals-triangle 3 2)
+(pascals-triangle 2 1)
 ;2
-(pascals-triangle 5 3)
+(pascals-triangle 4 0)
+;1
+(pascals-triangle 4 1)
+;4
+(pascals-triangle 4 2)
 ;6
+(pascals-triangle 4 3)
+;4
+(pascals-triangle 4 4)
+;1
 
 
