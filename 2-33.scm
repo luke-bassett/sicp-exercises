3b51786f54603b2bf7c2b24c68bfbca9eab19b03
Luke Bassett <bassett.luke@gmail.com>
2024-09-10 21:26:06 -0700

    thru 2.33 renamed

--- 2-33.scm
+++ 2-33.scm
@@ -6,22 +6,22 @@
     (op (car sequence)
         (accumulate op initial (cdr sequence)))))
 
-(define (map p sequence)
+(define (my-map p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
 
-(define (append seq1 seq2)
+(define (my-append seq1 seq2)
   (accumulate cons seq2 seq1))
 
-(define (length sequence)
+(define (my-len sequence)
   (accumulate (lambda (x rest-len) (+ 1 rest-len)) 0 sequence))
 
 
 ; test
 (define (square x)
   (* x x))
-(map square (list 2 3 4))
-(map (lambda (x) (* 3 x)) (list 2 3 4))
+(my-map square (list 2 3 4))
+(my-map (lambda (x) (* 3 x)) (list 2 3 4))
 
-(append (list 1 2) (list 69 420))
+(my-append (list 1 2) (list 69 420))
 
-(length (list 1 1 1))
+(my-len (list 1 1 1))
